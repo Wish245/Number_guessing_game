b56967091dd76ecf3b89544cf5d69631ca35299c
Wish245 <sankhawishwanath08@gmail.com>
2025-06-21 20:08:36 +0530

    Refactor number guessing game logic and improve user interaction

--- number_guess.py
+++ number_guess.py
@@ -4,12 +4,31 @@ print("Now I pick a number between 1 and 100")
 
 a = 1
 b = 100
-c = random.randint(a,b)
+target_number = random.randint(a,b)
 
-d = input("Now think of a number and put it here")
-d = int(d)
+guess_number = input("Now think of a number and put it here: ")
+guess_number = int(guess_number)
 
-if (c == d):
-    print(f"you pick {d} and I pick {c} you win")
-else:
-    print(f"I pick {c} hahahaha you loose")
+no_attempts = 0
+
+while (target_number!=guess_number):
+    guessing_value = target_number - guess_number
+    if(guessing_value >0 and guessing_value < 10):
+        guess_number = input("You are bit low but close guess again: ")
+        guess_number = int(guess_number)
+        no_attempts += 1
+    if(guessing_value >0 and guessing_value > 10):
+        guess_number = input("you are too low guess again: ")
+        guess_number = int(guess_number)
+        no_attempts += 1
+    if(guessing_value <0 and guessing_value < 10):
+        guess_number = input("You are bit high but close guess again: ")
+        guess_number = int(guess_number)
+        no_attempts += 1
+    if(guessing_value <0 and guessing_value > 10):
+        guess_number = input("you are too high guess again: ")
+        guess_number = int(guess_number)
+        no_attempts += 1
+print(f"you guessed correctly I guess {target_number} it took you {no_attempts} timess")
+
+    
